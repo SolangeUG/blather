997d116b79f25a54d5498788d79156a1756dab5b
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-12-06 10:14:22 +0000

    Use JDBC implementations of our repositories in the application

--- src/main/java/com/github/richardjwild/blather/application/ApplicationBuilder.java
+++ src/main/java/com/github/richardjwild/blather/application/ApplicationBuilder.java
@@ -1,15 +1,12 @@
 package com.github.richardjwild.blather.application;
 
 import com.github.richardjwild.blather.command.factory.*;
-import com.github.richardjwild.blather.helper.DatabaseConnection;
+import com.github.richardjwild.blather.helper.DataSourceManager;
 import com.github.richardjwild.blather.io.Input;
 import com.github.richardjwild.blather.io.Output;
-import com.github.richardjwild.blather.message.Message;
 import com.github.richardjwild.blather.message.MessageRepository;
 import com.github.richardjwild.blather.parsing.CommandReader;
 import com.github.richardjwild.blather.parsing.InputParser;
-import com.github.richardjwild.blather.persistence.InMemoryMessageRepository;
-import com.github.richardjwild.blather.persistence.InMemoryUserRepository;
 import com.github.richardjwild.blather.persistence.JdbcMessageRepository;
 import com.github.richardjwild.blather.persistence.JdbcUserRepository;
 import com.github.richardjwild.blather.persistence.dao.MessageDAO;
@@ -18,19 +15,20 @@ import com.github.richardjwild.blather.time.Clock;
 import com.github.richardjwild.blather.time.TimestampFormatter;
 import com.github.richardjwild.blather.user.UserRepository;
 
-import java.sql.Connection;
+import javax.sql.DataSource;
 
 public class ApplicationBuilder {
 
     public static Application build(Input input, Output output, Clock clock) {
 
-        //UserDAO userDAO = new UserDAO(DatabaseConnection.getConnection());
-        //MessageDAO messageDAO = new MessageDAO(DatabaseConnection.getConnection());
+        String propertiesFileName = "application-production.properties";
+        DataSource dataSource = new DataSourceManager(propertiesFileName).getDataSource();
 
-        UserRepository userRepository = new InMemoryUserRepository();
-        //new JdbcUserRepository(userDAO);
-        MessageRepository messageRepository = new InMemoryMessageRepository();
-        //new JdbcMessageRepository(messageDAO);
+        UserDAO userDAO = new UserDAO(dataSource);
+        MessageDAO messageDAO = new MessageDAO(dataSource);
+
+        UserRepository userRepository = new JdbcUserRepository(userDAO);
+        MessageRepository messageRepository = new JdbcMessageRepository(messageDAO);
 
         InputParser inputParser = new InputParser();
         Controller controller = new Controller();
